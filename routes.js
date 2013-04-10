28373e7a65dadf4ed6d636882b5489d80fb1897c
Noah Isaacson <noah@nisaacson.com>
2013-04-10 16:00:40 -0400

    added logout route

--- routes.js
+++ routes.js
@@ -1,3 +1,4 @@
+var passport = require('passport')
 var login = require('./routes/login')
 var register = require('./routes/register')
 var hub = require('./lib/hub')
@@ -21,7 +22,38 @@ module.exports = function (app) {
   app.get('/login', login)
   app.post('/login', login)
   app.get('/register', register)
-  app.post('/register', register)
+  app.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+      if (err) {
+        return next(err)
+      }
+      if (!user) {
+        // *** Display message using Express 3 locals
+        var message = 'Login Failed'
+        if (info && info.message) {
+          message = info.message
+        }
+        req.session.message = message
+        return res.redirect('/login')
+      }
+      req.logIn(user, function(err) {
+        if (err) {
+          return next(err)
+        }
+        req.session.password = req.body.password
+        return res.redirect('/')
+      })
+    })(req, res, next)
+  })
+
+  app.get('/logout', function (req, res) {
+    if (!req.user) {
+      return res.redirect('/')
+    }
+    req.logOut()
+    req.session.info = 'You have successfully logged out'
+    res.redirect('/')
+  })
 
   app.get('/hub', hub)
   app.get('/ps', ensureAuthenticated, ps)
