6836ab6df9490e8b3876919a7d94817ac30ecb4a
Noah Isaacson <noah@nisaacson.com>
2013-04-10 15:49:20 -0400

    add redis auth to startup

--- app.js
+++ app.js
@@ -13,44 +13,60 @@ module.exports = function(data, cb) {
   var sessionSecret = config.get('application:sessionSecret')
   var redisConfig = config.get('redis')
   var redisSessionClient = redis.createClient(redisConfig.port, redisConfig.host, {})
-  // redisSessionClient.auth(redisConfig.password)
-
-  app.set('views', __dirname + '/views')
-  app.set('view engine', 'jade')
-  app.use(express.cookieParser(cookieSecret))
-  app.use(connect.session({
-    store: new RedisStore({
-      client: redisSessionClient,
-      secret: sessionSecret
-    })
-  }))
-  app.use(express.bodyParser())
-  app.use(function(req, res, next) {
-    // Expose "error" and "message" to all views that are rendered.
-    res.locals.error = req.session.error || ''
-    res.locals.message = req.session.message || ''
-    res.locals.success = req.session.success || ''
-    // Remove them so they're not displayed on subsequent renders.
-    delete req.session.error
-    delete req.session.message
-    delete req.session.success
-    next()
-  })
-
-  app.use(ecstatic({
-    root: __dirname + '/public',
-    baseDir: '/static'
-  }))
-  app.use(app.router)
-  routes(app)
-
-
-  var server = http.createServer(app)
-  server.listen(port, function () {
-    var output = {
-      port: port,
-      server: server
+  authenticateRedis(redisSessionClient, function (err) {
+    if (err) {
+      return cb({
+        message: 'failed to start dispatch server, error authenticating redis client',
+        error: err,
+        stack: new Error().stack
+      })
     }
-    cb(null, output)
+    app.set('views', __dirname + '/views')
+    app.set('view engine', 'jade')
+    app.use(express.cookieParser(cookieSecret))
+    app.use(connect.session({
+      store: new RedisStore({
+        client: redisSessionClient,
+        secret: sessionSecret
+      })
+    }))
+    app.use(express.bodyParser())
+    app.use(function(req, res, next) {
+      // Expose "error" and "message" to all views that are rendered.
+      res.locals.error = req.session.error || ''
+      res.locals.message = req.session.message || ''
+      res.locals.success = req.session.success || ''
+      // Remove them so they're not displayed on subsequent renders.
+      delete req.session.error
+      delete req.session.message
+      delete req.session.success
+      next()
+    })
+
+    app.use(ecstatic({
+      root: __dirname + '/public',
+      baseDir: '/static'
+    }))
+    app.use(app.router)
+    routes(app)
+
+
+    var server = http.createServer(app)
+    server.listen(port, function () {
+      var output = {
+        port: port,
+        server: server
+      }
+      cb(null, output)
+    })
   })
+}
+
+function authenticateRedis(client, cb) {
+  var redisConfig = config.get('redis')
+  var password = redisConfig.password
+  if (!password) {
+    return cb()
+  }
+  client.auth(redisConfig.password, cb)
 }
